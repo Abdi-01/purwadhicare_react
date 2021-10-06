b2d8f274c20c64cd82bca77ec13b4ec36ad77191
reyhananda <reyhanandasetiawan@gmail.com>
2021-10-06 14:29:26 +0700

    PURC-2, PURC-8, PURC-9 & PURC-4, Membuat fitur Login, Register, Email Verification, Forgot Password, Change /Reset Password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Verification from "./pages/Verification";
+import ForgetPassword from "./pages/forgetPassword";
+import ResetPassword from "./pages/resetPassword";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route component={Login} path="/login" />
+          <Route component={Register} path="/register" />
+          <Route component={ResetPassword} path="/reset-password/:token" />
+          <Route component={ForgetPassword} path="/forget-password" />
+          <Route component={Verification} path="/authentication/:token" />
+          <Route component={Home} path="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
