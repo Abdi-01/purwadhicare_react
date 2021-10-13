8d05a02487947c41384e8dbf81e9a802ad4af2c0
sheillamariana <sheillapraiska@gmail.com>
2021-10-13 21:55:26 +0700

    FE PURC-16, PURC-17, PURC-18, PURC-19 (semua button add to cart belum berfungsi, menyusul di story edit cart)

--- src/pages/User/ProductDetail.jsx
+++ src/pages/User/ProductDetail.jsx
@@ -1,34 +1,83 @@
-import React from "react";
+import Axios from "axios"
+import React, { useState, useEffect } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
 
-function ProductDetail() {
+function ProductDetail(props) {
+
+  const [productData, setProductData] = useState({});
+  const [productNotFound, setProductNotFound] = useState(false)
+  const [quantity, setQuantity] = useState(1)
+  
+  const fetchProductData = () => {
+    // alert(props.match.params.productId)
+    Axios.get(`http://localhost:2200/product/get`, {
+      params : {
+        idproduct : props.match.params.idproduct}
+    })
+    .then((result)=>{
+      if (result.data.length) {
+        console.log(result.data[0])
+        // kalau tipe data sama data tidak perlu di spread
+        setProductData(result.data[0])
+      } else {
+        setProductNotFound(true)
+      }
+     
+    })
+    .catch(()=>{
+      alert("Terjadi kesalahan server")
+    })
+  }
+
+  const qtyBtnHandler = (action) => {
+    if (action === "increment") {
+      setQuantity(quantity +1 )
+    } else if (action === "decrement" && quantity > 1) {
+      setQuantity(quantity - 1)
+    }
+  }
+
+  const addToCartHandler = () => {
+    
+  }
+    // seperti component did mount
+    useEffect(() => {
+      fetchProductData();
+    }, []);
+
   return (
-    <div className="container">
-      <div className="row mt-3">
-        <div className="j col-6">
+    <div className="container p-5">
+      {
+        productNotFound ?
+        <div className="alert alert-info mt-5">Product not found</div> 
+        :
+        <div className="row p-5">
+        <div className="card product-card p-5 m-4 col-5">
           <img
-            style={{ width: "85%" }}
-            src="https://hdmall.id/system/image_attachments/images/000/008/725/medium/panadol-cold-flu-reg-tab-10s-hijau-1.jpg"
+            style={{ width: "100%" }}
+            src={productData.image}
             alt=""
           />
         </div>
 
         <div className="col-6 d-flex flex-column justify-content-center">
-          <h4>This Product Name </h4>
-          <h5>Rp. 19000,-</h5>
+          <h4>{productData.product_name}</h4>
+          <h5>{productData.price_stock}</h5>
           <p>
-            Product Description Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Suscipit sit at dicta perferendis, veniam, facilis
-            blanditiis perspiciatis ipsam qui eveniet dolores ratione distinctio
-            doloribus ipsum, repellat error enim natus? Aut?
+           {productData.description}
           </p>
           <div className="d-flex flex-row align-items-center justify-content-center my-3">
-            <button className="mr-4 rounded-circle btn btn-info">-</button>2
-            <button className="rounded-pill btn btn-info mx-4">+</button>
+            <button onClick={()=>qtyBtnHandler("decrement")} className="mr-4 rounded-circle btn btn-info">-</button>
+            {quantity}
+            <button onClick={()=>qtyBtnHandler("increment")} className="rounded-pill btn btn-info mx-4">+</button>
           </div>
-          <button className="btn btn-success mt-4">Add To Cart</button>
+          <button className="btn btn-info rounded-pill mt-4">Add To Cart</button>
         </div>
       </div>
-    </div>
+
+
+      }
+          </div>
   );
 }
 
