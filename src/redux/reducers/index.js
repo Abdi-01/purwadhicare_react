3a29f0f268fb87eeca80c1686b2d1b4290752066
sheillamariana <sheillapraiska@gmail.com>
2021-10-25 21:20:30 +0700

    mengosongkan cart checkout

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,6 @@ import { userReducer } from "./userReducers";
 import cartReducer from "./cart";
 
 export default combineReducers({
-  user: userReducer,
-  cart: cartReducer,
+    auth: userReducer,
+    cart: cartReducer,
 });
